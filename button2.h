754f7399e589d4a7a9dfd570cef890dbfd550a79
guyco75 <x>
2018-07-14 12:25:07 +0300

    rename SCENE to BUTTON_EVENT and other renaming and clean up

--- button2.h
+++ button2.h
@@ -1,134 +1,129 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-//#include "utils.h"
-#define assert(test, str1, str2)
-
-char btn_str[120];
-
 #define DEBOUNCE_INTERVAL_MS (40)             // Unstable/debounce duration
 #define BTN_LONG_PRESS_THRESHOLD_MS (500)     // Below that it's considered a SHORT press; above it a LONG press
 
-enum scene_fsm_state {
-  SCENE_FSM_IDLE,
-  SCENE_FSM_ARMED,
-  SCENE_FSM_POST_SHORT,
-  SCENE_FSM_LONG,
+enum button_fsm_state {
+  BUTTON_FSM_IDLE,
+  BUTTON_FSM_ARMED,
+  BUTTON_FSM_POST_SHORT,
+  BUTTON_FSM_LONG,
 };
-static const char *scene_fsm_state_names[] = {
-  "SCENE_FSM_IDLE",
-  "SCENE_FSM_ARMED",
-  "SCENE_FSM_POST_SHORT",
-  "SCENE_FSM_LONG",
+static const char *button_fsm_state_names[] = {
+  "BUTTON_FSM_IDLE",
+  "BUTTON_FSM_ARMED",
+  "BUTTON_FSM_POST_SHORT",
+  "BUTTON_FSM_LONG",
 };
 
-enum scene {
-  SCENE_NONE,
-  SCENE_ARMED,
-  SCENE_SINGLE_CLICK,
-  SCENE_SINGLE_CLICK_DONE,
-  SCENE_DOUBLE_CLICK,
-  SCENE_DOUBLE_CLICK_DONE,
-  SCENE_TRIPLE_CLICK,
-  SCENE_TRIPLE_CLICK_DONE,
-  SCENE_LONG_CLICK,
-  SCENE_UNDEF,
+enum button_event {
+  BUTTON_EVENT_NONE,
+  BUTTON_EVENT_ARMED,
+  BUTTON_EVENT_SINGLE_CLICK,
+  BUTTON_EVENT_SINGLE_CLICK_DONE,
+  BUTTON_EVENT_DOUBLE_CLICK,
+  BUTTON_EVENT_DOUBLE_CLICK_DONE,
+  BUTTON_EVENT_TRIPLE_CLICK,
+  BUTTON_EVENT_TRIPLE_CLICK_DONE,
+  BUTTON_EVENT_LONG_CLICK,
+  BUTTON_EVENT_UNDEF,
 };
-static const char *scene_names[] = {
-  "SCENE_NONE",
-  "SCENE_ARMED",
-  "SCENE_SINGLE_CLICK",
-  "SCENE_SINGLE_CLICK_DONE",
-  "SCENE_DOUBLE_CLICK",
-  "SCENE_DOUBLE_CLICK_DONE",
-  "SCENE_TRIPLE_CLICK",
-  "SCENE_TRIPLE_CLICK_DONE",
-  "SCENE_LONG_CLICK",
-  "SCENE_UNDEF",
+static const char *event_names[] = {
+  "BUTTON_EVENT_NONE",
+  "BUTTON_EVENT_ARMED",
+  "BUTTON_EVENT_SINGLE_CLICK",
+  "BUTTON_EVENT_SINGLE_CLICK_DONE",
+  "BUTTON_EVENT_DOUBLE_CLICK",
+  "BUTTON_EVENT_DOUBLE_CLICK_DONE",
+  "BUTTON_EVENT_TRIPLE_CLICK",
+  "BUTTON_EVENT_TRIPLE_CLICK_DONE",
+  "BUTTON_EVENT_LONG_CLICK",
+  "BUTTON_EVENT_UNDEF",
 };
 
 struct button {
   unsigned long last_change_ms;
   unsigned long action_start_time;
-  enum scene_fsm_state scene_fsm;
+  enum button_fsm_state fsm_state;
+  enum button_event last_event;
 
   volatile uint8_t *pin_port;
   uint8_t pin_bit;
   uint8_t clicks;
-  bool state;
-  bool unstable_state;
+  bool pin_state;
+  bool unstable_pin_state;
 
   void setup(uint8_t btn_pin) {
     this->pin_port = portInputRegister(digitalPinToPort(btn_pin));
     this->pin_bit = digitalPinToBitMask(btn_pin);
     pinMode(btn_pin, INPUT_PULLUP);
 
-    state = unstable_state = 0;
+    pin_state = unstable_pin_state = 0;
     clicks = 0;
     last_change_ms = millis();
   }
 
-  void change_fsm_state(enum scene_fsm_state st) {
+  void change_fsm_state(enum button_fsm_state st) {
 #if 0
-    snprintf(btn_str, sizeof(btn_str), "%lu\t\t%s --> %s", millis(), scene_fsm_state_names[scene_fsm], scene_fsm_state_names[st]);
+    snprintf(btn_str, sizeof(btn_str), "%lu\t\t%s --> %s", millis(), button_fsm_state_names[fsm_state], button_fsm_state_names[st]);
     Serial.println(btn_str);
 #endif
-    scene_fsm = st;
+    fsm_state = st;
   }
 
-  enum scene read_state() {
+  enum button_event fsm() {
     bool read_val = !(*pin_port & pin_bit);
 
-    if (read_val != unstable_state) {
-      unstable_state = read_val;
+    if (read_val != unstable_pin_state) {
+      unstable_pin_state = read_val;
       last_change_ms = millis();
     } else {
-      if (read_val != state && (millis() - last_change_ms > DEBOUNCE_INTERVAL_MS)) {
-        state = read_val;
+      if (read_val != pin_state && (millis() - last_change_ms > DEBOUNCE_INTERVAL_MS)) {
+        pin_state = read_val;
       }
     }
 
-    switch(scene_fsm) {
-      case SCENE_FSM_IDLE:
-        if (state) {
+    switch(fsm_state) {
+      case BUTTON_FSM_IDLE:
+        if (pin_state) {
           action_start_time = millis();
-          change_fsm_state(SCENE_FSM_ARMED);
+          change_fsm_state(BUTTON_FSM_ARMED);
           clicks = 0;
-          return SCENE_ARMED;
+          return BUTTON_EVENT_ARMED;
         }
-        return SCENE_NONE;
+        return BUTTON_EVENT_NONE;
 
-      case SCENE_FSM_ARMED:
-        if (!state) { // short click
-          change_fsm_state(SCENE_FSM_POST_SHORT);
+      case BUTTON_FSM_ARMED:
+        if (!pin_state) { // short click
+          change_fsm_state(BUTTON_FSM_POST_SHORT);
           action_start_time = millis();
-          return (enum scene)(clicks < 3 ? SCENE_SINGLE_CLICK + 2*clicks : SCENE_UNDEF);
+          return (enum button_event)(clicks < 3 ? BUTTON_EVENT_SINGLE_CLICK + 2*clicks : BUTTON_EVENT_UNDEF);
         } else if ((millis() - action_start_time) >= BTN_LONG_PRESS_THRESHOLD_MS) { // long click
-          change_fsm_state(SCENE_FSM_LONG);
-          return SCENE_LONG_CLICK;
+          change_fsm_state(BUTTON_FSM_LONG);
+          return BUTTON_EVENT_LONG_CLICK;
         }
-        return SCENE_NONE;
+        return BUTTON_EVENT_NONE;
 
-      case SCENE_FSM_POST_SHORT:
-        if (state) {
+      case BUTTON_FSM_POST_SHORT:
+        if (pin_state) {
           action_start_time = millis();
-          change_fsm_state(SCENE_FSM_ARMED);
+          change_fsm_state(BUTTON_FSM_ARMED);
           clicks++;
-          return SCENE_ARMED;
+          return BUTTON_EVENT_ARMED;
         } else if ((millis() - action_start_time) >= BTN_LONG_PRESS_THRESHOLD_MS) {
-          change_fsm_state(SCENE_FSM_IDLE);
-          return (enum scene)(clicks < 3 ? SCENE_SINGLE_CLICK_DONE + 2*clicks : SCENE_UNDEF);
+          change_fsm_state(BUTTON_FSM_IDLE);
+          return (enum button_event)(clicks < 3 ? BUTTON_EVENT_SINGLE_CLICK_DONE + 2*clicks : BUTTON_EVENT_UNDEF);
         }
-        return SCENE_NONE;
+        return BUTTON_EVENT_NONE;
 
-      case SCENE_FSM_LONG:
-        if (state) {
-          return SCENE_LONG_CLICK;
+      case BUTTON_FSM_LONG:
+        if (pin_state) {
+          return BUTTON_EVENT_LONG_CLICK;
         }
-        change_fsm_state(SCENE_FSM_IDLE);
-        return SCENE_NONE;
+        change_fsm_state(BUTTON_FSM_IDLE);
+        return BUTTON_EVENT_NONE;
     }
-    assert(false, "invalid scene_fsm state", scene_fsm);
   }
 };
 
